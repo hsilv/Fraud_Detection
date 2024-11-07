5cb5a14a83cbdade1b365f2eb1b50707271cc9e8
hsilv <hm.sebastiansilva@gmail.com>
2024-11-07 11:31:44 -0600

    finish analysis

--- st.py
+++ st.py
@@ -188,3 +188,152 @@ chart = alt.Chart(fraud_count_by_month).mark_bar(color='skyblue', size=15).encod
 
 # Mostrar gráfico en Streamlit
 st.altair_chart(chart, use_container_width=True)
+
+st.divider()
+
+fraud_class_distribution = df['is_fraud'].value_counts().reset_index()
+fraud_class_distribution.columns = ['class', 'count']
+fraud_class_distribution['class'] = fraud_class_distribution['class'].map({0: 'No Fraude', 1: 'Fraude'})
+
+# Gráfico con Altair
+chart = alt.Chart(fraud_class_distribution).mark_bar().encode(
+    x=alt.X('class', title='Clase'),
+    y=alt.Y('count', title='Cantidad de Transacciones'),
+    color=alt.Color('class', scale=alt.Scale(domain=['No Fraude', 'Fraude'], range=['skyblue', 'salmon']))
+).properties(
+    width=600,
+    height=400,
+    title='Distribución de Clases de Fraude y No Fraude'
+).configure_axis(
+    labelFontSize=12,
+    titleFontSize=14
+).configure_title(
+    fontSize=16
+).configure_axisY(
+    grid=True,
+    gridOpacity=0.7,
+    gridDash=[5, 5]
+)
+
+# Mostrar gráfico en Streamlit
+st.altair_chart(chart, use_container_width=True)
+
+st.divider()
+
+# Datos
+data = df[['amt']]
+
+# Gráfico con Altair
+chart = alt.Chart(data).mark_bar(color='skyblue', stroke='black').encode(
+    alt.X('amt:Q', bin=alt.Bin(maxbins=50), title='Monto de Transacción (amt)'),
+    alt.Y('count()', title='Frecuencia')
+).properties(
+    width=600,
+    height=400,
+    title='Distribución de las Transacciones con respecto al Monto (amt)'
+).configure_axis(
+    labelFontSize=12,
+    titleFontSize=14
+).configure_title(
+    fontSize=16
+).configure_axisY(
+    grid=True,
+    gridOpacity=0.7,
+    gridDash=[5, 5]
+)
+
+# Mostrar gráfico en Streamlit
+st.altair_chart(chart, use_container_width=True)
+
+high_value_transactions_count = df[df['amt'] > 1500].shape[0]
+high_value_fraud_count = df[(df['amt'] > 1500) & (df['is_fraud'] == 1)].shape[0]
+df = df[df['amt'] <= 1500]
+data = df[['amt']]  # Asegurarse de que data sea un DataFrame
+
+# Gráfico con Altair
+chart = alt.Chart(data).mark_bar(color='skyblue', stroke='black').encode(
+    alt.X('amt:Q', bin=alt.Bin(maxbins=50), title='Monto de Transacción (amt)'),
+    alt.Y('count()', title='Frecuencia')
+).properties(
+    width=600,
+    height=400,
+    title='Distribución de las Transacciones con respecto al Monto (amt)'
+).configure_axis(
+    labelFontSize=12,
+    titleFontSize=14
+).configure_title(
+    fontSize=16
+).configure_axisY(
+    grid=True,
+    gridOpacity=0.7,
+    gridDash=[5, 5]
+)
+
+# Mostrar gráfico en Streamlit
+st.altair_chart(chart, use_container_width=True)
+
+# Lista de variables a graficar
+variables = ['category', 'gender', 'state']
+
+# Graficar la frecuencia de transacciones para cada variable
+for var in variables:
+    # Preparar los datos
+    data = df[var].value_counts().reset_index()
+    data.columns = [var, 'count']
+    
+    # Gráfico con Altair
+    chart = alt.Chart(data).mark_bar(color='skyblue', stroke='black').encode(
+        x=alt.X(f'{var}:N', title=var.capitalize(), axis=alt.Axis(labelAngle=-45), sort='-y'),
+        y=alt.Y('count:Q', title='Frecuencia de Transacciones')
+    ).properties(
+        width=600,
+        height=400,
+        title=f'Frecuencia de Transacciones con respecto a la variable {var.capitalize()}'
+    ).configure_axis(
+        labelFontSize=12,
+        titleFontSize=14
+    ).configure_title(
+        fontSize=16
+    ).configure_axisY(
+        grid=True,
+        gridOpacity=0.7,
+        gridDash=[5, 5]
+    )
+    
+    # Mostrar gráfico en Streamlit
+    st.altair_chart(chart, use_container_width=True)
+
+#creamos una columna de edad, utilizando el variable de fecha de nacimiento
+df['age'] = df['trans_date_trans_time'].dt.year - df['dob'].dt.year
+
+# Ajustar la edad si la persona aún no cumplió años ese año
+df['age'] -= ((df['trans_date_trans_time'].dt.month < df['dob'].dt.month) |
+              ((df['trans_date_trans_time'].dt.month == df['dob'].dt.month) &
+               (df['trans_date_trans_time'].dt.day < df['dob'].dt.day)))
+
+# Eliminar la columna 'dob'
+df = df.drop(columns=['dob'])
+
+data = df[['age']]
+
+# Gráfico con Altair
+chart = alt.Chart(data).mark_bar(color='skyblue', stroke='black').encode(
+    alt.X('age:Q', bin=alt.Bin(maxbins=50), title='Edad (años)'),
+    alt.Y('count()', title='Frecuencia')
+).properties(
+    width=600,
+    height=400,
+    title='Distribución de las Transacciones con respecto a la edad'
+).configure_axis(
+    labelFontSize=12,
+    titleFontSize=14
+).configure_title(
+    fontSize=16
+).configure_axisY(
+    grid=True,
+    gridOpacity=0.7,
+    gridDash=[5, 5]
+)
+
+# Mostrar gráfico en Streamlit
+st.altair_chart(chart, use_container_width=True)
